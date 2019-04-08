8f38836ba87b28b75d712a42bcdaf0ef051ee2aa
mymayday <1157840315@qq.com>
2019-04-08 15:17:32 +0800

    config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ configer.modelbase = 'recognize_mobilefacenet'
 
 configer.datatype = 'Multi'
 if configer.datatype == 'Multi':
-    configer.usedChannels = [850]
+    configer.usedChannels = [550]
     configer.n_usedChannels = len(configer.usedChannels)
     configer.modelname = '{}_{}_{}chs_{}sta_20nm'.\
                     format(configer.modelbase, configer.splitmode, configer.n_usedChannels, configer.usedChannels[0])
