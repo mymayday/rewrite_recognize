ff653b92a7efda7fbc8e3bdd41b570d72b559d41
siminzhu <1157840315@qq.com>
2019-04-12 20:30:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -9,14 +9,14 @@ configer.n_class = 63
 
 
 configer.splitmode = 'split_{}x{}_2'.format(configer.dsize[0], configer.dsize[1])
-#configer.modelbase = 'recognize_mobilefacenet'
+configer.modelbase = 'recognize_mobilefacenet'
 #configer.modelbase ='recognize_mobilenet'
-configer.modelbase = 'recognize_vgg11_bn'
+#configer.modelbase = 'recognize_vgg11_bn'
  
 
 configer.datatype = 'Multi'
 if configer.datatype == 'Multi':
-    configer.usedChannels =[810]
+    configer.usedChannels =[750]
     #configer.usedChannels = range(550,1000)[::20]
     configer.n_usedChannels = len(configer.usedChannels)
     configer.modelname = '{}_{}_{}chs_{}sta_20nm'.\
