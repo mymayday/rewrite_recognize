80fafae6b0c99956f0ed601bd8f1a67821ad9e28
siminzhu <1157840315@qq.com>
2019-04-20 16:31:21 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ configer.n_channels = 23
 configer.n_class = 63
 
 
-configer.splitmode = 'split_{}x{}_41'.format(configer.dsize[0], configer.dsize[1])
+configer.splitmode = 'split_{}x{}_42'.format(configer.dsize[0], configer.dsize[1])
 #configer.modelbase = 'recognize_mobilefacenet'
 configer.modelbase ='recognize_mobilenet'
 #configer.modelbase = 'recognize_vgg11_bn'
