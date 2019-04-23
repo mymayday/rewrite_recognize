824500c7ede08e107ebfbf2d6541c320f2bfacfe
siminzhu <1157840315@qq.com>
2019-04-23 21:46:40 +0800

    Update preinitmodel.py

--- preinitmodel.py
+++ preinitmodel.py
@@ -1,9 +1,15 @@
 import os
 import torch
 from models.mobilenet import MobileNet
+import plt
 
 preinit="./init.pkl"
+net=MobileNet()
 
 if not os.path.axist(preinit):
+  torch.save(net.state_dict(),preinit)
+else:
+  net.state_dict(torch.load(preinit))
 
+plt.show(preinit)
 
