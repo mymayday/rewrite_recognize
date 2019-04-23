e2b7170af60ed7148b17adfe704ba990e1f73763
siminzhu <1157840315@qq.com>
2019-04-23 21:19:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ configer.n_channels = 23
 configer.n_class = 63
 
 
-configer.splitmode = 'split_{}x{}_4'.format(configer.dsize[0], configer.dsize[1])
+configer.splitmode = 'split_{}x{}_5'.format(configer.dsize[0], configer.dsize[1])
 #configer.modelbase = 'recognize_mobilefacenet'
 configer.modelbase ='recognize_mobilenet'
 #configer.modelbase = 'recognize_shufflev2'
